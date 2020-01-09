f0b39518401a2ed1afd66eaf82c22f56e5111155
jeremy_plott <plott.j@pg.com>
2020-01-09 13:46:09 -0500

    Add project files.

---
+++ LCLongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LCLongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "pwwkew";
+
+            HashSet<char> substringHash = new HashSet<char>();
+
+            int longestSubstring = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int substringLength = 0;
+
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (substringHash.Contains(s[j]))
+                    {
+                        break;
+                    }
+                    substringHash.Add(s[j]);
+                    substringLength++;
+                }
+                if (substringLength > longestSubstring)
+                {
+                    longestSubstring = substringLength;
+                }
+                substringHash.Clear();
+            }
+
+            Console.WriteLine(longestSubstring);
+
+        }
+    }
+}
